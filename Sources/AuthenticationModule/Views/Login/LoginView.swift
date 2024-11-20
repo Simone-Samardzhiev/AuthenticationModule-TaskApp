22e913e18f14293f5f56a2a1d7a52adcb54d141c
simonesamardzhiev <simonesamardzhiev@gmail.com>
2024-11-20 14:00:38 +0200

    Add LoginView.swift

---
+++ Sources/AuthenticationModule/Views/Login/LoginView.swift
@@ -0,0 +1,24 @@
+//
+//  LoginView.swift
+//  AuthenticationModule
+//
+//  Created by Simone Samardzhiev on 20.11.24.
+//
+
+import SwiftUI
+
+/// View that will show the login
+public struct LoginView: View {
+    /// Default initializer.
+    public init() {
+        
+    }
+    
+    public var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    LoginView()
+}
